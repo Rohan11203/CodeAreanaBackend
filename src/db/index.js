7e49cb6154519eb97cacfbdf938d510b55528d43
Rohan <rohanshikhare410@gmail.com>
2024-10-23 14:24:31 +0530

    %

--- src/db/index.js
+++ src/db/index.js
@@ -18,11 +18,16 @@ const pool = new Pool({
 module.exports = {
   query: async (text, params) => {
     try {
+      console.log("Query text:", text);
+      console.log("Query params:", params);
+      
       const result = await pool.query(text, params);
+      console.log("Query result:", result);
+      
       return result;
     } catch (err) {
-      console.error('Database query error:', err.stack);
-      throw err;
+      console.error('Database query error1:', err.stack);  // Log the complete error stack
+      throw err;  // Re-throw the error for the higher-level catch to handle it
     }
   },
 };
