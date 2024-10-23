0b06728d5a7425fb0de25abcb450f8e02283d787
Rohan <rohanshikhare410@gmail.com>
2024-10-23 13:30:00 +0530

    %

--- src/index.js
+++ src/index.js
@@ -23,8 +23,8 @@ const authRoutes = require('./routes/auth')
 //initialize routes
 app.use('/api',authRoutes)
 
-  app.get('/test', (req, res) => {
-    res.render('Test request sent')
+  app.get('/api/test', (req, res) => {
+    res.send('Test request sent')
   })
 // app start
 // const appStart = () =>{
